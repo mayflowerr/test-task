3c4260e87e22ad36393ca79c01a7b86848436e44
Ilya <mayprogg@gmail.com>
2025-09-26 18:53:21 +0300

    Add base api views for Question and Answer

--- drf_app/main/views.py
+++ drf_app/main/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from main.models import Question, Answer
+from main.serializers import (
+    QuestionSerializer, 
+    AnswerSerializer, 
+    QuestionWithAnswersSerializer
+)
 
-# Create your views here.
+
+class QuestionListCreateView(generics.ListCreateAPIView):
+    queryset = Question.objects.order_by("-id")
+    serializer_class = QuestionSerializer
+
+
+class QuestionRetrieveDeleteView(generics.RetrieveDestroyAPIView):
+    queryset = Question.objects.order_by("-id")
+    serializer_class = QuestionWithAnswersSerializer\
+    
+
+class AnswerCreateView(generics.CreateAPIView):
+    serializer_class = AnswerSerializer
+
+    def create(self, request, *args, **kwargs):
+        q = get_object_or_404(Question, pk=kwargs["pk"])
+        data = request.data.copy()
+        ser = self.get_serializer(data=data)
+        ser.is_valid(raise_exception=True)
+        ser.save(question=q)
+        return Response(ser.data, status=status.HTTP_201_CREATED)
+    
+
+class AnswerRetrieveDeleteView(generics.RetrieveDestroyAPIView):
+    queryset = Answer.objects.order_by("-id")
+    serializer_class = AnswerSerializer
