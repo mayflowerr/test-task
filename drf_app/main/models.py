725e41df6233520f8361f42b3a4ffd4a1982da8b
Ilya <mayprogg@gmail.com>
2025-09-26 18:41:53 +0300

    Add Question and Answer models

--- drf_app/main/models.py
+++ drf_app/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.TextField('Текст')
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+    
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers', verbose_name='Вопрос')
+    user_id = models.CharField('ID пользователя', max_length=64)
+    text = models.TextField('Текст')
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+
+    def __str__(self):
+        return f'{self.user_id}: {self.text}'
